e089ab1cc358589eb805bf310ab2121a08aa0895
Matt Gaidica, Ph.D <matt@gaidi.ca>
2024-09-30 15:16:43 -0500

    re-adv after disconnect working

--- ESP32_Node.ino
+++ ESP32_Node.ino
@@ -2,6 +2,8 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+// #include <SD.h>
+// #include <SPI.h>
 
 #define RGB_BRIGHTNESS 10  // Change white brightness (max 255)
 #ifdef RGB_BUILTIN
@@ -12,13 +14,16 @@
 #define SERVICE_UUID "12345678-1234-1234-1234-123456789abc"
 #define CHARACTERISTIC_UUID_FILELIST "87654321-4321-4321-4321-abcdefabcdf1"      // To receive file list from Pi
 #define CHARACTERISTIC_UUID_FILETRANSFER "87654321-4321-4321-4321-abcdefabcdf2"  // For file data
+#define SD_CS_PIN 5                                                              // Pin for SD card
 
 BLECharacteristic *pFileListCharacteristic;
 BLECharacteristic *pFileTransferCharacteristic;
 BLEServer *pServer;  // Global BLE server object
 bool deviceConnected = false;
+std::vector<String> fileListOnPi;
 
-// Server callback functions to track connection status
+// File dataFile;  // Commented until SD functionality is needed
+
 class ServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer *pServer) {
     deviceConnected = true;
@@ -28,6 +33,10 @@ class ServerCallbacks : public BLEServerCallbacks {
   void onDisconnect(BLEServer *pServer) {
     deviceConnected = false;
     Serial.println("Device disconnected.");
+
+    // Restart advertising after disconnection
+    BLEDevice::getAdvertising()->start();
+    Serial.println("Advertising restarted.");
   }
 };
 
@@ -40,12 +49,12 @@ class FileListCallback : public BLECharacteristicCallbacks {
       Serial.println(value);
 
       // Parse received file list (assumes filenames are separated by commas)
-      // fileListOnPi.clear();
-      // char *token = strtok((char *)value.c_str(), ",");
-      // while (token != NULL) {
-      //   fileListOnPi.push_back(String(token));  // Using Arduino String
-      //   token = strtok(NULL, ",");
-      // }
+      fileListOnPi.clear();
+      char *token = strtok((char *)value.c_str(), ",");
+      while (token != NULL) {
+        fileListOnPi.push_back(String(token));  // Using Arduino String
+        token = strtok(NULL, ",");
+      }
     }
   }
 };
@@ -55,6 +64,13 @@ void setup() {
   Serial.begin(115200);
   delay(2000);
   Serial.println("Starting ESP32 BLE...");
+
+  // Initialize the SD card
+  // if (!SD.begin(SD_CS_PIN)) {
+  //   Serial.println("SD Card initialization failed!");
+  //   return;
+  // }
+  // Serial.println("SD Card initialized.");
 
   // Initialize BLE
   BLEDevice::init("ESP32_BLE_SD");
@@ -82,12 +98,50 @@ void setup() {
   // Start advertising using legacy BLE advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->start();
-  Serial.println("BLE advertising started. Waiting for client connection...");
+  Serial.println("BLE advertising started.");
 }
 
 void loop() {
-  neopixelWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0);  // Green
+  if (deviceConnected) {
+    Serial.println("Device Connected!");
+    //   // Once connected, loop through SD card files and transfer new ones
+    //   File root = SD.open("/");
+    //   transferFiles(root);
+  }
+
+  neopixelWrite(RGB_BUILTIN, 0, 0, RGB_BRIGHTNESS);  // Blue
   delay(200);                                        // Adjust loop delay as needed
   neopixelWrite(RGB_BUILTIN, 0, 0, 0);               // Turn off LED
   delay(200);
+}
+
+// Function to handle file transfer
+void transferFiles(/*File dir*/) {
+  // while (true) {
+  //   File entry = dir.openNextFile();
+  //   if (!entry) break;
+
+  //   // Check if the filename is in the fileListOnPi vector
+  //   if (std::find(fileListOnPi.begin(), fileListOnPi.end(), entry.name()) == fileListOnPi.end()) {
+  //     Serial.printf("Transferring file: %s\n", entry.name());
+
+  //     // Send filename to the Pi
+  //     pFileTransferCharacteristic->setValue(entry.name());
+  //     pFileTransferCharacteristic->notify();
+  //     delay(100);  // Let Pi register filename
+
+  //     // Send file contents
+  //     while (entry.available()) {
+  //       String dataLine = entry.readStringUntil('\n');
+  //       pFileTransferCharacteristic->setValue(dataLine.c_str());
+  //       pFileTransferCharacteristic->notify();
+  //       delay(100);  // Adjust delay based on transfer speed
+  //     }
+
+  //     entry.close();
+  //   }
+  // }
+
+  // Optionally disconnect to conserve power after all files are transferred
+  // pServer->disconnect(0);
 }

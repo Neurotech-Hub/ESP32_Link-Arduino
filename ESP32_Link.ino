186e21129e0c292c5e24032e97d057046b839f82
Matt Gaidica, Ph.D. <matt@gaidi.ca>
2024-10-17 17:01:36 -0500

    1ms drops large files, 10ms works, will need stress testing

--- ESP32_Link.ino
+++ ESP32_Link.ino
@@ -20,7 +20,7 @@ const int cs = 10;
 #define CHARACTERISTIC_UUID_FILENAME "87654321-4321-4321-4321-abcdefabcdf3"      // R/W/I characteristic for filenames
 #define CHARACTERISTIC_UUID_FILETRANSFER "87654321-4321-4321-4321-abcdefabcdf2"  // R/I characteristic for file transfer
 uint16_t mtuSize = 20;  // Default MTU size, updated after negotiation
-#define NOTIFICATION_DELAY 1  // ms
+#define NOTIFICATION_DELAY 10  // ms
 
 BLECharacteristic *pFilenameCharacteristic;
 BLECharacteristic *pFileTransferCharacteristic;
